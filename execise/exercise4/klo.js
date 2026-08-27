d1bca6d8b1d164885971d522110f8d707680b8ea
mohamed-zaa <towin011@gmail.com>
2026-08-27 17:36:08 +0300

    Add exercise 4

---
+++ execise/exercise4/klo.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+// 1
+
+function add(a,b){
+    return a+b
+}
+console.log(add(6,1))
+console.log(add(4,6))
+console.log(add(6,4))
+console.log(add(6,0))
+
+
+
+
+
+
+// 2
+
+
+let add = function(b,h){
+    return b+h
+}
+console.log(add(6,46))
+console.log(add(6,37))
+console.log(add(6,9))
