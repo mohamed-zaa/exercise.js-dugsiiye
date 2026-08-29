b2c4645faa10081e7cfc1ee0642f0291811437ea
mohamed-zaa <towin011@gmail.com>
2026-08-29 10:37:26 +0300

    Add exercise 7

---
+++ execise/exercise7/kala.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+let colors = ["green", "blue", "red" ]
+
+    console.log(colors[0])
+      console.log(colors[1])
+        console.log(colors[2])
+
+// push
+
+        colors.push("brown")
+        console.log(colors)
+
+
+        // shift
+
+        colors.shift()
+        console.log(colors)
