a10c89e92e5c1390e6b7c9a1e06605fc6bb9dc31
mohamed-zaa <towin011@gmail.com>
2026-08-29 23:31:24 +0300

    Add exercise 17

---
+++ execise/exercise17/17.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+let tem = 55
+
+if(tem < 0){
+    console.log("is very cold")
+
+}
+else if(tem <= 15){
+console.log("is cold")
+}
+else if(tem <= 25){
+console.log("is warm")
+}
+else {
+console.log("is hot")
+}
