c6e3627397b51421cba3e64dd2011ceb4f12d542
mohamed-zaa <towin011@gmail.com>
2026-08-29 11:42:49 +0300

    Add exercise 10

---
+++ execise/exercise10/10.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+let library = [
+    {
+
+        title : "akhris wanaag",
+        author : "ali hasan bare",
+        yaer : 2020,
+
+
+
+    },
+    {
+
+
+        title : "socod",
+        author : "asmo faarax",
+        yaer : 2025,
+
+
+
+
+
+    },
+    {
+
+
+
+        
+        title : "hage cafimad",
+        author : "faarax kalid ali",
+        yaer : 2010,
+
+
+
+    },
+]
+
+console.log(library[0].title)
+
+console.log(library[1].author)
